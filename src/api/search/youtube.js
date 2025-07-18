45543388d5d94c59eef2cefff73f6e3aac5039e0
rijalganzz <keyshan430@gmail.com>
2025-07-18 23:12:27 +0700

    Update youtube.js

--- src/api/search/youtube.js
+++ src/api/search/youtube.js
@@ -1,35 +1,41 @@
 module.exports = function (app) {
 
+app.get('/search/ytmp3', async (req, res) => {
+    try {
+        const { url } = req.query
+        if (!url) return res.json({ status: false, error: 'Url is required' })
+
+        const results = await global.fetchJson(`https://zenz.biz.id/downloader/ytmp3?url=${encodeURIComponent(url)}`)
+
+        res.status(200).json({
+            status: true,
+            title: results.title,
+            duration: results.duration,
+            thumbnail: results.thumbnail,
+            format: results.format,
+            type: results.type,
+            download_url: results.download_url,
+            creator: results.creator
+        })
+    } catch (error) {
+        res.status(500).send(`Error: ${error.message}`)
+    }
+})
+
 app.get('/search/ytmp4', async (req, res) => {
     try {
-        const { url } = req.query;
-        if (!url) return res.json({ status: false, error: 'Url is required' });
+        const { url } = req.query
+        if (!url) return res.json({ status: false, error: 'Url is required' })
 
-        const results = await global.fetchJson(`https://fastrestapis.fasturl.cloud/downup/ytmp4?url=${url}&quality=720&server=auto`);
+        const results = await global.fetchJson(`https://fastrestapis.fasturl.cloud/downup/ytmp4?url=${url}&quality=720&server=auto`)
 
         res.status(200).json({
             status: true,
             result: results.result
-        });
+        })
     } catch (error) {
-        res.status(500).send(`Error: ${error.message}`);
+        res.status(500).send(`Error: ${error.message}`)
     }
-});
-
-app.get('/search/ytmp3', async (req, res) => {
-    try {
-        const { url } = req.query;
-        if (!url) return res.json({ status: false, error: 'Url is required' });
-
-        const results = await global.fetchJson(`https://api.siputzx.my.id/api/dl/youtube/mp3?url=${encodeURIComponent(url)}`);
-
-        res.status(200).json({
-            status: true,
-            result: results.data
-        });
-    } catch (error) {
-        res.status(500).send(`Error: ${error.message}`);
-    }
-});
+})
 
 }
