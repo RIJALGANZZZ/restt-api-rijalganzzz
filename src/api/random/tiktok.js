5ed47fcef814f7189dc1728853170e435efeabcc
rijalganzz <keyshan430@gmail.com>
2025-07-19 14:58:17 +0700

    Create tiktok.js

---
+++ src/api/random/tiktok.js
@@ -0,0 +1,19 @@
+const axios = require("axios")
+
+module.exports = function(app) {
+  app.get("/random/tiktok", async (req, res) => {
+    try {
+      const { url } = req.query
+      if (!url) return res.json({ status: false, error: "Url is required" })
+
+      const response = await axios.get(`https://izumi-apis.zone.id/downloader/tiktokdl?url=${encodeURIComponent(url)}`)
+      res.status(200).json({
+        status: true,
+        creator: "RijalGanzz",
+        result: response.data
+      })
+    } catch (error) {
+      res.status(500).send(`Error: ${error.message}`)
+    }
+  })
+    }
