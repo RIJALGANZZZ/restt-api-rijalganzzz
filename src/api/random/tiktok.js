7394da53bfdbc0705eaa18153de8dfa0830a7cac
rijalganzz <keyshan430@gmail.com>
2025-07-19 14:59:57 +0700

    Update tiktok.js

--- src/api/random/tiktok.js
+++ src/api/random/tiktok.js
@@ -1,7 +1,7 @@
 const axios = require("axios")
 
 module.exports = function(app) {
-  app.get("/random/tiktok", async (req, res) => {
+  app.get("/download/tiktok", async (req, res) => {
     try {
       const { url } = req.query
       if (!url) return res.json({ status: false, error: "Url is required" })
@@ -10,10 +10,10 @@ module.exports = function(app) {
       res.status(200).json({
         status: true,
         creator: "RijalGanzz",
-        result: response.data
+        result: response.data.result
       })
     } catch (error) {
       res.status(500).send(`Error: ${error.message}`)
     }
   })
-    }
+}
