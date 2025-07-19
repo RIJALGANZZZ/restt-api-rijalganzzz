c7ba127671a37374bb4bbf6bf1a4a56ff463057e
rijalganzz <keyshan430@gmail.com>
2025-07-19 19:08:20 +0700

    Create mediafire.js

---
+++ src/api/random/mediafire.js
@@ -0,0 +1,20 @@
+module.exports = function (app) {
+app.get('/random/mediafire', async (req, res) => {
+    try {
+        const { url } = req.query;
+        if (!url) return res.json({ status: false, error: 'Url is required' });
+
+        const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
+        const response = await fetch(`https://zenz.biz.id/downloader/mediafire?url=${encodeURIComponent(url)}`);
+        const data = await response.json();
+
+        res.status(200).json({
+            status: true,
+            creator: 'RijalGanzz',
+            result: data.result
+        });
+    } catch (error) {
+        res.status(500).send(`Error: ${error.message}`);
+    }
+});
+    }
